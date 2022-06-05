0b1146393e984d7b497ec40cd47c83d28ab9790c
Gabriel-Jeronimo <gabrieljeronimo-@hotmail.com>
2022-06-05 15:52:33 -0300

    Add JWT authentication

--- nexus-typegen.ts
+++ nexus-typegen.ts
@@ -28,6 +28,10 @@ export interface NexusGenScalars {
 }
 
 export interface NexusGenObjects {
+  AuthPayload: { // root type
+    token: string; // String!
+    user: NexusGenRootTypes['User']; // User!
+  }
   Link: { // root type
     description: string; // String!
     id: number; // Int!
@@ -53,6 +57,10 @@ export type NexusGenRootTypes = NexusGenObjects
 export type NexusGenAllTypes = NexusGenRootTypes & NexusGenScalars
 
 export interface NexusGenFieldTypes {
+  AuthPayload: { // field return type
+    token: string; // String!
+    user: NexusGenRootTypes['User']; // User!
+  }
   Link: { // field return type
     description: string; // String!
     id: number; // Int!
@@ -62,7 +70,9 @@ export interface NexusGenFieldTypes {
   Mutation: { // field return type
     UpdateLink: NexusGenRootTypes['Link']; // Link!
     deleteLink: NexusGenRootTypes['Link']; // Link!
+    login: NexusGenRootTypes['AuthPayload']; // AuthPayload!
     post: NexusGenRootTypes['Link']; // Link!
+    signup: NexusGenRootTypes['AuthPayload']; // AuthPayload!
   }
   Query: { // field return type
     feed: NexusGenRootTypes['Link'][]; // [Link!]!
@@ -77,6 +87,10 @@ export interface NexusGenFieldTypes {
 }
 
 export interface NexusGenFieldTypeNames {
+  AuthPayload: { // field return type name
+    token: 'String'
+    user: 'User'
+  }
   Link: { // field return type name
     description: 'String'
     id: 'Int'
@@ -86,7 +100,9 @@ export interface NexusGenFieldTypeNames {
   Mutation: { // field return type name
     UpdateLink: 'Link'
     deleteLink: 'Link'
+    login: 'AuthPayload'
     post: 'Link'
+    signup: 'AuthPayload'
   }
   Query: { // field return type name
     feed: 'Link'
@@ -110,9 +126,18 @@ export interface NexusGenArgTypes {
     deleteLink: { // args
       id: number; // Int!
     }
+    login: { // args
+      email: string; // String!
+      password: string; // String!
+    }
     post: { // args
       description: string; // String!
       url: string; // String!
+    }
+    signup: { // args
+      email: string; // String!
+      name: string; // String!
+      password: string; // String!
     }
   }
   Query: {
